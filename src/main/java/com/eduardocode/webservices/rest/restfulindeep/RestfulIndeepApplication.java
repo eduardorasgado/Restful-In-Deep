eefc789c923d132390a4b361ee5839145db6636b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-22 18:55:16 -0500

    spring based web service project created.

---
+++ src/main/java/com/eduardocode/webservices/rest/restfulindeep/RestfulIndeepApplication.java
@@ -0,0 +1,13 @@
+package com.eduardocode.webservices.rest.restfulindeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulIndeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulIndeepApplication.class, args);
+    }
+
+}
