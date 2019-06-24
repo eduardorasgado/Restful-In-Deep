18ce0272e653919b74c108352ee30c6864ff984b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-24 01:19:58 -0500

    Exception package created. Response general exception class created to represent data retreived to client for exception handler reporting. User not found exception class moved.

---
+++ src/main/java/com/eduardocode/webservices/rest/restfulindeep/exception/ResponseGeneralException.java
@@ -0,0 +1,24 @@
+package com.eduardocode.webservices.rest.restfulindeep.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * <h1>ResponseGeneralException</h1>
+ * General exception response structure to send error information to cliente
+ * Used as message container in {@link CustomizedResponseEntityExceptionHandler}
+ *
+ * @author Eduardo Rasgado Ruiz
+ * @see CustomizedResponseEntityExceptionHandler
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResponseGeneralException {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
--- src/main/java/com/eduardocode/webservices/rest/restfulindeep/exception/UserNotFoundException.java
+++ src/main/java/com/eduardocode/webservices/rest/restfulindeep/exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.eduardocode.webservices.rest.restfulindeep.error;
+package com.eduardocode.webservices.rest.restfulindeep.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
