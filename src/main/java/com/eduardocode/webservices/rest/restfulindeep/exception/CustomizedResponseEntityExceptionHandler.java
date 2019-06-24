cd85499ad5117838ed4e2997235811ea178c28f5
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-24 02:31:02 -0500

    customized response rest controller created and applied to all resources. general exception and user not found exception handler implemented in above rest controller.

---
+++ src/main/java/com/eduardocode/webservices/rest/restfulindeep/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.eduardocode.webservices.rest.restfulindeep.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+/**
+ * <h1>CustomizedResponseEntityExceptionHandler</h1>
+ * Class to represent a global exception handler to be applied to all controllers in application
+ * It uses {@link ResponseGeneralException} as virtual object with error data to be returned to client
+ * It is a rest controller and it is applied to every resource available in application
+ *
+ * @author Eduardo Rasgado Ruiz
+ * @see ResponseGeneralException
+ */
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * Method to handle all the exceptions can occur in resources
+     * @param ex
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request)
+            throws Exception {
+        //
+        ResponseGeneralException rge = new ResponseGeneralException(new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(rge, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Method to send a 404 not found status and {@link ResponseGeneralException} with not found
+     * information directly as server response
+     * @param ex
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(
+            Exception ex, WebRequest request) throws Exception {
+        ResponseGeneralException rge = new ResponseGeneralException(new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(rge, HttpStatus.NOT_FOUND);
+    }
+}
