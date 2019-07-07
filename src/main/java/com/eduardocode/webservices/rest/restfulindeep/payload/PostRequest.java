960abdd1aec904198f25d5c45ba4c216c4f82b64
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-07-06 23:54:54 -0500

    i18n in post request validation messages.

--- src/main/java/com/eduardocode/webservices/rest/restfulindeep/payload/PostRequest.java
+++ src/main/java/com/eduardocode/webservices/rest/restfulindeep/payload/PostRequest.java
@@ -18,13 +18,11 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostRequest {
-    @NotBlank(message = "El titulo no puede ser vacío")
-    @Size(min = 10, message = "El titulo debe contener al menos 10 caracteres")
-    @Size(max = 40, message = "El titulo no puede tener mas de 40 caracteres")
+    @NotBlank
+    @Size(min = 10, max = 40)
     private String title;
-    @NotBlank(message = "El contenido no puede estar vacio")
-    @Size(min = 20, message = "El contenido debe ser al menos de 20 caracteres")
-    @Size(max = 500, message = "El contenido no puede tener mas de 500 caracteres")
+    @NotBlank
+    @Size(min = 20, max = 500)
     private String content;
     private Set<String> tags;
 }
