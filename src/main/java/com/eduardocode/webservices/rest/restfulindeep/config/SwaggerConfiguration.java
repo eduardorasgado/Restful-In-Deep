094505a9302affa0224ab9376ce34db0e5054291
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-07-09 00:52:07 -0500

    swagger documentation info in swagger config bean

--- src/main/java/com/eduardocode/webservices/rest/restfulindeep/config/SwaggerConfiguration.java
+++ src/main/java/com/eduardocode/webservices/rest/restfulindeep/config/SwaggerConfiguration.java
@@ -2,9 +2,14 @@ package com.eduardocode.webservices.rest.restfulindeep.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * <h1>SwaggerConfiguration</h1>
@@ -16,12 +21,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "Aaron Landeros", "", "aaron.landeros@gmamil.com");
+
+    private final ApiInfo CUSTOM_API_INFO = new ApiInfo(
+            "My Company Documentation",
+            "This api has been developed by a awesome company",
+            "1.0",
+            "urn:tos",
+                         DEFAULT_CONTACT,
+            "MIT",
+            "",
+            new ArrayList<VendorExtension>());
     /**
      * This method configures the default options for documentation
      * @return swagger docket
      */
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(CUSTOM_API_INFO);
     }
 }
